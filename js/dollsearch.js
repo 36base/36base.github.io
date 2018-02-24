07ca8ecfee8e4164c747bd26e0e3b1f17d420089
36DB <bradje@naver.com>
2018-02-25 03:59:51 +0900

    Add files via upload

--- js/dollsearch.js
+++ js/dollsearch.js
@@ -14,9 +14,9 @@ function searchFunction() {
 			div[i].style.display="none";
 		}
 	}
-	for (i=0; i<div.length; i++) {
-		if (c[i].innerHTML.toUpperCase().indexOf(filter)>-1){
-			div[i].style.display="";
+	for (i = 0; i< div.length; i++) {
+		if (c[i].innerHTML.toUpperCase().indexOf(filter)>-1) {
+			div[i].style.display ="";
 		}
 		else {
 			div[i].style.display="none";
