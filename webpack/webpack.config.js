57ae956a1caea46371b54fe94d3f0bf041509fbc
CirnoV <cirnov59@gmail.com>
2018-02-25 12:51:27 +0900

    Enable eslint

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,48 +1,48 @@
 const path = require('path');
 const UglifyPlugin = require('uglifyjs-webpack-plugin');
 
-const proj_dir = path.resolve(__dirname, '..');
+const projDir = path.resolve(__dirname, '..');
 
 module.exports = {
-    entry: {
-        app: path.resolve(proj_dir, 'src/index.jsx')
-    },
+  entry: {
+    app: path.resolve(projDir, 'src/index.jsx'),
+  },
 
-    resolve: {
-        modules: [path.resolve(proj_dir, 'src'), 'node_modules'],
-        extensions: ['.js', '.jsx']
-    },
+  resolve: {
+    modules: [path.resolve(projDir, 'src'), 'node_modules'],
+    extensions: ['.js', '.jsx'],
+  },
 
-    output: {
-        path: proj_dir,
-        filename: 'bundle.js'
-    },
+  output: {
+    path: projDir,
+    filename: 'bundle.js',
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)?$/,
-                exclude: /node_modules/,
-                use: ['babel-loader', 'eslint-loader']
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          require.resolve('style-loader'),
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: '[path][name]__[local]--[hash:base64:5]',
             },
-            {
-                test: /\.css$/,
-                use: [
-                    require.resolve('style-loader'),
-                    {
-                      loader: require.resolve('css-loader'),
-                      options: {
-                        importLoaders: 1,
-                        modules: true,
-                        localIdentName: '[path][name]__[local]--[hash:base64:5]'
-                      },
-                    }
-                ]
-            }
-        ]
-    },
+          },
+        ],
+      },
+    ],
+  },
 
-    plugins: [
-        new UglifyPlugin()
-    ]
+  plugins: [
+    new UglifyPlugin(),
+  ],
 };
