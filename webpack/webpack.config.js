b8a46ad854b952ba153d74da2a670f0f5d586b5b
angrypepsi <angrypepsi>
2018-03-01 19:43:32 +0900

    revert

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   output: {
     path: path.resolve(projDir, 'assets'),
     filename: 'js/bundle.js',
-    publicPath: 'https://angrypepsi.github.io/36db.github.io/assets',
+    publicPath: 'http://girlsfrontline.kr/assets',
   },
 
   module: {
