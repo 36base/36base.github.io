88cef563340d57f312adcda345a17008259f1296
CirnoV <cirnov59@gmail.com>
2018-03-01 12:41:18 +0900

    Cleanup

--- origin/stash/csswriter.py
+++ origin/stash/csswriter.py
@@ -1,11 +1,16 @@
 import os
 
-data = open('result.txt', 'w')
+data = open('result1.txt', 'w')
 css=''
 
-for i in range(1, 211):
+for i in range(1001, 1009):
 	css = '.doll-link a#number' + str(i) + """ {
- background-image: url(../images/portrait/""" + str(i) + """.png);
+ background-image: url(../images/dolls/""" + str(i) + """.png);
+  background-position: 54% 4%;
+}
+
+.doll-link a#number""" + str(i) + """:hover {
+  background-image: url(..//images/dolls/""" + str(i) + """_1.png);
 }
 
 """
