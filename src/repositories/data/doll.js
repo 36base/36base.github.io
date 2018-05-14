caa90f6b7d78ac490044d6d237b4e332c8321da7
okms00909 <okms00909@gmail.com>
2018-05-15 02:58:15 +0900

    asd

--- src/repositories/data/doll.js
+++ src/repositories/data/doll.js
@@ -1,11 +1,13 @@
 import { dolls } from 'girlsfrontline-core';
 import dollRanks from './dollRank';
 import dollTypes from './dollType';
+import dollSkills from './dollSkill';
 import dollSpines from './dollSpines';
 
 const domain = 'https://girlsfrontline.kr/hotlink-ok/girlsfrontline-resources/images';
 const typeMap = new Map(dollTypes.map(e => [e.code, e]));
 const rankMap = new Map(dollRanks.map(e => [e.id, e]));
+const skillMap = new Map(dollSkills.map((e, i) => [i, e]));
 const spineMap = new Map(Object.keys(dollSpines).map(k => [Number(k), dollSpines[k]]));
 
 function getTypeIcon(typeId, rankId) {
@@ -40,9 +42,32 @@ function buildImage(id, skins, spine) {
   ];
 }
 
+function buildSkill(skill) {
+  const base = skillMap.get(skill.id) || {};
+  const dataPool = new Map(base.data.map(e => [e.key, { ...e, values: skill.dataPool[e.key] }]));
+
+  return {
+    id: skill.id,
+    name: base.name,
+    template: base.desc,
+    icon: `${domain}/skill/${base.path}.png`,
+    dataPool,
+  };
+}
+
 const dollList = dolls.map((doll) => {
   const rank = doll.id > 1000 ? 1 : doll.rank;
   const spine = spineMap.get(doll.id);
+
+  const skill = {
+    id: 6,
+    initCooldown: 6,
+    dataPool: {
+      PW: [12, 13, 14, 15, 16, 17, 18, 19, 20, 22],
+      DR: [5, 6, 6, 6, 7, 7, 7, 8, 8, 8],
+      CD: [15, 14.7, 14.3, 14, 13.7, 13.3, 13, 12.7, 12.3, 12],
+    },
+  };
 
   return {
     id: doll.id,
@@ -59,7 +84,7 @@ const dollList = dolls.map((doll) => {
     images: buildImage(doll.id, doll.skins, spine),
     stats: doll.stats,
     effect: doll.effect,
-    skill: doll.getSkill(),
+    skill: buildSkill(skill),
     acquisition: {
       build: doll.buildTime,
       drop: doll.drop,
