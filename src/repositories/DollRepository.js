bf5457019059560ac805f73f503cbc4ff10a6fb8
okms00909 <okms00909@gmail.com>
2018-06-13 14:52:40 +0900

    fix bug

--- src/repositories/DollRepository.js
+++ src/repositories/DollRepository.js
@@ -74,14 +74,14 @@ const dollList = dolls.map((doll) => {
       spineCode: spine ? spine.code : undefined,
       icon: getTypeIcon(doll.type, rank),
       portrait: `${domain}/portraits/${doll.id}.png`,
-      images: buildImage(doll.id, doll.skins, spine),
+      images: buildImage(doll.id, doll.skins || [], spine),
       stats: doll.stats,
       effect: doll.effect,
       skill: buildSkill(doll.skill),
       getSkill: doll.getSkill,
       acquisition: {
         build: doll.buildTime,
-        drop: doll.drop,
+        drop: doll.drop || [],
       },
     };
 
