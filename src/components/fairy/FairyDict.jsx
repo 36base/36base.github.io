9d94203b1548dd0268676e3437417b6595e5d1b4
chojaeho <real0131@gmail.com>
2018-06-21 22:31:40 +0900

    fix fairyDict layout

--- src/components/fairy/FairyDict.jsx
+++ src/components/fairy/FairyDict.jsx
@@ -8,11 +8,55 @@ import FairyCard from './components/FairyCard';
 // import SearchBar from './components/SearchBar';
 
 
-const style = {
+const style = theme => ({
   wrapper: {
     width: '100%',
+    [theme.breakpoints.down(2046)]: {
+      width: '1520px',
+      margin: '0 auto',
+    },
+    [theme.breakpoints.down(1856)]: {
+      width: '1330px',
+      margin: '0 auto',
+    },
+    [theme.breakpoints.down(1666)]: {
+      width: '1140px',
+      margin: '0 auto',
+    },
+    [theme.breakpoints.down(1559)]: {
+      width: '1520px',
+      margin: '0 auto',
+    },
+    [theme.breakpoints.down(1536)]: {
+      width: '1330px',
+      margin: '0 auto',
+    },
+    [theme.breakpoints.down(1346)]: {
+      width: '1140px',
+      margin: '0 auto',
+    },
+    [theme.breakpoints.down(1156)]: {
+      width: '950px',
+      margin: '0 auto',
+    },
+    [theme.breakpoints.down(966)]: {
+      width: '760px',
+      margin: '0 auto',
+    },
+    [theme.breakpoints.down(776)]: {
+      width: '570px',
+      margin: '0 auto',
+    },
+    [theme.breakpoints.down(586)]: {
+      width: '380px',
+      margin: '0 auto',
+    },
+    [theme.breakpoints.down(480)]: {
+      width: '320px',
+      margin: '0 auto',
+    },
   },
-};
+});
 // {list.map(fairy => <FairyCard key={fairy.id} {...fairy} />)}
 /*
 <Grid container>
