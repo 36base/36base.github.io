a86f10c237704f1dd08e6849d8b6a168506e78fe
chojaeho <real0131@gmail.com>
2018-08-08 22:48:45 +0900

    fix combat report calculator translation (ja)

--- src/locale/ja/locale.js
+++ src/locale/ja/locale.js
@@ -71,7 +71,7 @@ export default {
   'OATH': '誓約',
   'Current EXP.': '現在経験値',
   'Target Level': '目標レベル',
-  'Require amount :': '必要な作戦報告書',
+  'Require amount': '必要な作戦報告書',
   'fairy': 'フェアリー',
   'doll': 'ドール',
   'amount': '個',
