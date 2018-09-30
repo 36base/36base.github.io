70dce4669e707b6a3a1a1c07b617926574ac42d1
okms00909 <okms00909@gmail.com>
2018-09-30 23:49:46 +0900

    change resourceHost in production mode to gitraw (temporary)

--- src/utils/host.js
+++ src/utils/host.js
@@ -3,5 +3,6 @@ export function getResourceHost(env = process.env.NODE_ENV) {
     // TODO: 나중에 branch를 master로 바꿔야함
     return 'https://raw.githubusercontent.com/36base/girlsfrontline-resources/next/';
   }
-  return 'https://girlsfrontline.kr/hotlink-ok/girlsfrontline-resources/';
+  return 'https://raw.githubusercontent.com/36base/girlsfrontline-resources/next/';
+  // return 'https://girlsfrontline.kr/hotlink-ok/girlsfrontline-resources/';
 }
