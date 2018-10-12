632c87f67f02bf88beeca3d1de33d33c34fa0cd7
okms00909 <okms00909@gmail.com>
2018-10-13 04:05:39 +0900

    modify test tobe

--- src/utils/host.spec.js
+++ src/utils/host.spec.js
@@ -2,12 +2,12 @@ import { getResourceHost } from './host';
 
 describe('`getResourceHost()`', () => {
   test('returns rawgit host', () => {
-    expect(getResourceHost()).toBe('https://raw.githubusercontent.com/36base/girlsfrontline-resources/next/');
+    expect(getResourceHost()).toBe('https://raw.githubusercontent.com/36base/girlsfrontline-resources/master/');
   });
   test('returns rawgit host when NODE_ENV is "development"', () => {
-    expect(getResourceHost('development')).toBe('https://raw.githubusercontent.com/36base/girlsfrontline-resources/next/');
+    expect(getResourceHost('development')).toBe('https://raw.githubusercontent.com/36base/girlsfrontline-resources/master/');
   });
   test('returns hotlink host when NODE_ENV is "production"', () => {
-    expect(getResourceHost('production')).toBe('https://girlsfrontline.kr/hotlink-ok/girlsfrontline-resources/');
+    expect(getResourceHost('production')).toBe('https://girlsfrontline.kr/data/resource/');
   });
 });
--- src/utils/url.spec.js
+++ src/utils/url.spec.js
@@ -3,15 +3,15 @@ import { getDollResourceUrl, getDollTypeIconUrl, getFairyTypeIconUrl } from './u
 describe('`getDollResourceUrl()`', () => {
   test('returns normal image url', () => {
     expect(getDollResourceUrl('IDWMod', 'normal'))
-      .toBe('https://raw.githubusercontent.com/36base/girlsfrontline-resources/next/pic/pic_IDWMod.png');
+      .toBe('https://raw.githubusercontent.com/36base/girlsfrontline-resources/master/pic/pic_IDWMod.png');
   });
   test('returns damaged image url', () => {
     expect(getDollResourceUrl('IDWMod', 'damaged', { skin: 2108 }))
-      .toBe('https://raw.githubusercontent.com/36base/girlsfrontline-resources/next/pic/pic_IDW_2108_D.png');
+      .toBe('https://raw.githubusercontent.com/36base/girlsfrontline-resources/master/pic/pic_IDW_2108_D.png');
   });
   test('returns portrait image url', () => {
     expect(getDollResourceUrl('IDWMod', 'portrait', { skin: 2108 }))
-      .toBe('https://raw.githubusercontent.com/36base/girlsfrontline-resources/next/pic/pic_IDW_2108_N_mod.png');
+      .toBe('https://raw.githubusercontent.com/36base/girlsfrontline-resources/master/pic/pic_IDW_2108_N_mod.png');
   });
   test('throws error when received unexpected type', () => {
     expect(() => getDollResourceUrl('IDWMod', 'DANYAAAAAAAAA', { skin: 2108 }))
@@ -21,12 +21,12 @@ describe('`getDollResourceUrl()`', () => {
 
 describe('`getDollTypeIconUrl()`', () => {
   test('returns typeicon url', () => {
-    expect(getDollTypeIconUrl('ar', 7)).toBe('https://raw.githubusercontent.com/36base/girlsfrontline-resources/next/typeicons/gun/AR7.png');
+    expect(getDollTypeIconUrl('ar', 7)).toBe('https://raw.githubusercontent.com/36base/girlsfrontline-resources/master/typeicons/gun/AR7.png');
   });
 });
 
 describe('`getFairyTypeIconUrl()`', () => {
   test('returns typeicon url', () => {
-    expect(getFairyTypeIconUrl('battle')).toBe('https://raw.githubusercontent.com/36base/girlsfrontline-resources/next/typeicons/fairy/battle.png');
+    expect(getFairyTypeIconUrl('battle')).toBe('https://raw.githubusercontent.com/36base/girlsfrontline-resources/master/typeicons/fairy/battle.png');
   });
 });
